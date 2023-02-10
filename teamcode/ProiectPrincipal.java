c75cb5d20d320389b641f6e0f89ace97a1f790d0
Maxim Sabin <sabin@disroot.org>
2023-02-10 15:58:34 +0200

    Update ProiectPrincipal.java

--- teamcode/ProiectPrincipal.java
+++ teamcode/ProiectPrincipal.java
@@ -84,16 +84,23 @@ public class ProiectPrincipal extends LinearOpMode {
 			} else if (gamepad2.dpad_down) {
 				// belt.moveBelt(Constants.IntakeTargets.UP);
 				lift.moveLift(Constants.LiftTargets.PICKUP);
-			} else if (gamepad2.left_stick_y < -0.01) {
+			// }
+			// else if (gamepad2.x){
+			// 	if(lift.getPosition()<4000)
+			// 		lift.moveLift(lift.getPosition() + 150);
+			// }
+			// else if (gamepad2.y){
+			// 	if(lift.getPosition()>150)
+			// 		lift.moveLift(lift.getPosition() - 150);
+			// }
+			} else if (gamepad2.left_stick_y < -0.05) {
 				if (lift.getPosition() < 4000)
 					lift.moveLift(lift.getPosition() + (int) (150 * (-gamepad2.left_stick_y)));
-			} else if (gamepad2.left_stick_y > 0.01) {
+			} else if (gamepad2.left_stick_y > 0.05) {
 				if (lift.getPosition() > 150)
 					lift.moveLift(lift.getPosition() - (int) (150 * (gamepad2.left_stick_y)));
-				else if (lift.getPosition() > 20)
-					lift.moveLift(lift.getPosition() - 20); // aici e plus fiindca LSY devine negativ
-				else if (lift.getPosition() > 2)
-					lift.moveLift(lift.getPosition() - 2); // control mai bun
+				else if (lift.getPosition() > 29)
+					lift.moveLift(lift.getPosition() - 15); // control mai bun
 			}
 
 			// ---
@@ -106,12 +113,12 @@ public class ProiectPrincipal extends LinearOpMode {
 					servo_gheara.setPosition(Range.clip(0.07, MIN_POSITION, MAX_POSITION));
 					// TimeUnit.MILLISECONDS.sleep(30);
 				} else {
-					servo_gheara.setPosition(Range.clip(0.18, MIN_POSITION, MAX_POSITION));
+					servo_gheara.setPosition(Range.clip(0.13, MIN_POSITION, MAX_POSITION));
 					// TimeUnit.MILLISECONDS.sleep(30);
 				}
-				TimeUnit.MILLISECONDS.sleep(100);
+				TimeUnit.MILLISECONDS.sleep(250);
 			}
-
+			telemetry.addData("gheara pozitie", servo_gheara.getPosition());
 			// ---
 
 			// --- gamepad 1 directie
@@ -121,13 +128,27 @@ public class ProiectPrincipal extends LinearOpMode {
 			// motorFrontRight.setPower(frontRightPower);
 			// motorBackRight.setPower(backRightPower);
 
-			double r = Math.hypot(-gamepad1.left_stick_x, gamepad1.left_stick_y);
-			double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
-			double rightX = -gamepad1.right_stick_x;
+			// JOYSTICK
+			// fata = -
+			// spate = +
+			// stanga = -
+			// dreapta = +
+			
+			double r = Math.hypot(-gamepad1.right_stick_x, gamepad1.left_stick_y);
+			double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.right_stick_x) - Math.PI / 4;
+			double rightX = -gamepad1.left_trigger;
 			final double v1 = r * Math.cos(robotAngle) + rightX;
 			final double v2 = r * Math.sin(robotAngle) - rightX;
 			final double v3 = r * Math.sin(robotAngle) + rightX;
 			final double v4 = r * Math.cos(robotAngle) - rightX;
+
+			// double r = Math.hypot(-gamepad1.left_stick_x, gamepad1.left_stick_y);
+			// double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
+			// double rightX = -gamepad1.right_stick_x;
+			// final double v1 = r * Math.cos(robotAngle) + rightX;
+			// final double v2 = r * Math.sin(robotAngle) - rightX;
+			// final double v3 = r * Math.sin(robotAngle) + rightX;
+			// final double v4 = r * Math.cos(robotAngle) - rightX;
 
 			motorFrontLeft.setPower(v1);
 			motorFrontRight.setPower(v2);
@@ -136,12 +157,18 @@ public class ProiectPrincipal extends LinearOpMode {
 
 			// ---
 
+			telemetry.addData("GP1LX", gamepad1.left_stick_x);
+			telemetry.addData("GO1LY", gamepad1.left_stick_y);
+			telemetry.addData("GP2LX", gamepad2.left_stick_x);
+			telemetry.addData("GO2LY", gamepad2.left_stick_y);
+			// telemetry.update();
+
 			// uncomment:
 			// telemetry.addData("Mode", "running");
 			// telemetry.addData("servo1 power", " = " + servo1.getPower());
 			// telemetry.addData("servo2 power", " = " + servo2.getPower());
 			// telemetry.addData("servo3 power", " = " + servo3.getPower());
-			// telemetry.addData("Lift Position", lift.getPosition());
+			telemetry.addData("Lift Position", lift.getPosition());
 			// telemetry.addData("A2", gamepad2.a); // gheara
 			// // -- lift
 			// telemetry.addData("Dpad2 Up", gamepad2.dpad_up);
@@ -149,7 +176,7 @@ public class ProiectPrincipal extends LinearOpMode {
 			// telemetry.addData("Dpad2 down", gamepad2.dpad_down);
 			// telemetry.addData("Dpad2 left", gamepad2.dpad_left);
 			// // --
-			// telemetry.update();
+			telemetry.update();
 		}
 	}
 }
