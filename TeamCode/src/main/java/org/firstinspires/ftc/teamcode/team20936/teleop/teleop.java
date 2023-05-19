ff685963d05c0058295938b2cd4c428d23d207d8
Electronica C.N."Stefan cel Mare" Suceava <91566119+ET-CNSCM@users.noreply.github.com>
2023-05-19 18:52:18 +0300

    Update teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team20936/teleop/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team20936/teleop/teleop.java
@@ -10,6 +10,7 @@ import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.controller.PIDFController;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -22,11 +23,10 @@ import org.firstinspires.ftc.teamcode.team20936.teleop.subsystems.Lift;
 @TeleOp
 public class teleop extends LinearOpMode{
 
-    int val_de_dat_la_lift = 0;
-
-    double MIN_POSITION = 0;
-    double MAX_POSITION = 1;
+    int lift_height = 0;
     boolean gheara_toggle = true;
+    boolean brat_rev_toggle = true;
+    boolean brat_sus_toggle = true;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -34,68 +34,64 @@ public class teleop extends LinearOpMode{
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
+        Servo servo_brat2 = hardwareMap.servo.get("servo_brat2"); servo_brat2.setPosition(Range.clip(0.37, 0, 1));
+        Servo servo_brat3 = hardwareMap.servo.get("servo_brat3"); servo_brat3.setPosition(Range.clip(0.5, 0, 1));
+        Servo servo_gheara = hardwareMap.servo.get("servo_gheara"); servo_gheara.setPosition(Range.clip(0.3, 0, 1));
+
+        Servo servo_deposit = hardwareMap.servo.get("servo_deposit"); servo_deposit.setPosition(Range.clip(0, 0, 1));
+
+        DistanceSensor sensorDistanta_intake = hardwareMap.get(DistanceSensor.class, "senzor_gheara");
+
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft"); motorFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft"); motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight"); motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight"); motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        DcMotor rev_hd_brat = hardwareMap.dcMotor.get("rev_hd_brat"); rev_hd_brat.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        rev_hd_brat.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         Lift lift = new Lift();
-
-        PIDFController pidf = new PIDFController(0.37, 0.05, 1.02, 0.7);
-
-        GamepadEx controller1 = new GamepadEx(gamepad1);
-
-        Servo servo_gheara = hardwareMap.servo.get("servo_gheara");
-        Servo servo_brat1 = hardwareMap.servo.get("servo_brat1");
-        Servo servo_brat2 = hardwareMap.servo.get("servo_brat2");
-        DistanceSensor sensorDistanta = hardwareMap.get(DistanceSensor.class, "senzor_gheara");
-
-
-        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
-        DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
-        DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
-        DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
-        // ---
-
-//        Motor motorFrontRight = new Motor(hardwareMap, "motorFrontRight", Motor.GoBILDA.RPM_312);
-//        Motor motorFrontLeft = new Motor(hardwareMap, "motorFrontLeft", Motor.GoBILDA.RPM_312);
-//        Motor motorBackRight = new Motor(hardwareMap, "motorBackRight", Motor.GoBILDA.RPM_312);
-//        Motor motorBackLeft = new Motor(hardwareMap, "motorBackLeft", Motor.GoBILDA.RPM_312);
-
-//        motorFrontLeft.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
-//        motorBackLeft.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
-//        motorFrontRight.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
-//        motorBackRight.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
-
-
         motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        motorFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-//        MecanumDrive mecanum = new MecanumDrive (motorFrontLeft, motorFrontRight, motorBackLeft, motorBackRight);
-
-        servo_brat1.setPosition(Range.clip(0.55, MIN_POSITION, MAX_POSITION));
-        servo_brat2.setPosition(Range.clip(0.5, MIN_POSITION, MAX_POSITION));
-        servo_gheara.setPosition(Range.clip(0.3, MIN_POSITION, MAX_POSITION));
-
+        lift.init(hardwareMap);
         waitForStart();
 
-        lift.init(hardwareMap);
 
         while (opModeIsActive())
         {
-            // --- Moving lift
+
+            if (gamepad2.a) {
+                if (brat_rev_toggle) {
+                    rev_hd_brat.setTargetPosition(-420);
+                    rev_hd_brat.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                    rev_hd_brat.setPower(0.1);
+                    brat_rev_toggle = !brat_rev_toggle;
+                }
+                else {
+                    rev_hd_brat.setTargetPosition(2);
+                    rev_hd_brat.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                    rev_hd_brat.setPower(-0.5);
+                    brat_rev_toggle = !brat_rev_toggle;
+                }
+            }
+            if (gamepad1.options) {
+                if (brat_sus_toggle) {
+                    servo_deposit.setPosition(Range.clip(0, 0, 1));
+                    brat_sus_toggle = !brat_sus_toggle;
+                }
+                else {
+                    servo_deposit.setPosition(Range.clip(0.23, 0, 1));
+                    brat_sus_toggle = !brat_sus_toggle;
+                }
+            }
+
+            telemetry.addData("Status", "Running");
+
             if (gamepad1.a) {
-                val_de_dat_la_lift = 1;
-//                lift.moveLift(Constants.LiftTargets.HIGH);
+                lift_height = 1;
             } else if (gamepad1.b) {
-                val_de_dat_la_lift = 2;
-//                lift.moveLift(Constants.LiftTargets.LOW);
+                lift_height = 2;
             } else if (gamepad1.y) {
-                val_de_dat_la_lift = 3;
-//                lift.moveLift(Constants.LiftTargets.MEDIUM);
-//            } else if (gamepad1.dpad_down) {
-                // belt.moveBelt(Constants.IntakeTargets.UP);
-//                lift.moveLift(Constants.LiftTargets.PICKUP);
+                lift_height = 3;
             } else if (gamepad1.right_trigger > 0.01 && lift.getPosition()<2650)
                     lift.moveLift(lift.getPosition() + 80 * (Math.abs((int) gamepad1.right_trigger)));
              else if (gamepad1.left_trigger > 0.01) {
@@ -104,7 +100,7 @@ public class teleop extends LinearOpMode{
             }
 
             if (gamepad1.left_bumper)
-                switch (val_de_dat_la_lift) {
+                switch (lift_height) {
                     case 1: {
                         lift.moveLift(Constants.LiftTargets.LOW);
                         break;
@@ -121,53 +117,79 @@ public class teleop extends LinearOpMode{
             else if (gamepad1.right_bumper)
                 lift.moveLift(1);
 
-            telemetry.addData("Status", "Running");
-
-//            mecanum.driveRobotCentric(controller1.getRightX(),
-//                    -controller1.getLeftY(),
-//                    controller1.getLeftX());
-
-            if(servo_gheara.getPosition() < 0.3)
+            if(servo_gheara.getPosition() > 0.2 && sensorDistanta_intake.getDistance(DistanceUnit.MM) < 30)
             {
-                if(sensorDistanta.getDistance(DistanceUnit.MM) < 50)
-                {
                     gheara_toggle = !gheara_toggle;
-                    servo_gheara.setPosition(Range.clip(0.0, MIN_POSITION, MAX_POSITION));
+                    servo_gheara.setPosition(Range.clip(0.0, 0, 1));
+                    sleep(200);
+                    servo_brat2.setPosition(Range.clip(0.26,0 , 1));
+                    servo_brat3.setPosition(Range.clip(0.5, 0, 1));
 
                     TimeUnit.MILLISECONDS.sleep(250);
 
-                    gamepad1.rumble(200);
+                    gamepad2.rumble(200);
                     telemetry.addData("auto pickup", "picked");
-                }
             }
-
-
-            if (gamepad1.x)
+            if (gamepad2.right_bumper)
             {
 
                 gheara_toggle = !gheara_toggle;
-                if (gheara_toggle) { servo_gheara.setPosition(Range.clip(0.3, MIN_POSITION, MAX_POSITION)); }
-                else { servo_gheara.setPosition(Range.clip(0.0, MIN_POSITION, MAX_POSITION)); TimeUnit.MILLISECONDS.sleep(400);} // DESCHIS ( SE EXECUTA PRIMA)
+                if (gheara_toggle) { servo_gheara.setPosition(Range.clip(0.3, 0, 1)); }
+                else { servo_gheara.setPosition(Range.clip(0.0, 0, 1)); TimeUnit.MILLISECONDS.sleep(400);} // DESCHIS ( SE EXECUTA PRIMA)
 
                 TimeUnit.MILLISECONDS.sleep(250);
 
-
             }
+
+            if(gamepad2.right_trigger > 0.3)
+            {
+                servo_brat2.setPosition(Range.clip(0.37,0,1));
+                servo_brat3.setPosition(Range.clip(0.5, 0, 1));
+            }
+
+            if(gamepad2.right_stick_y > 0.01 && servo_brat2.getPosition() >= 0)
+            {
+                double dist = Math.abs(gamepad2.right_stick_y) * 0.01;
+
+                servo_brat2.setPosition(Range.clip(servo_brat2.getPosition() - dist, 0, 1)) ;
+            }
+            else if(gamepad2.right_stick_y < 0.01 && servo_brat2.getPosition() <= 0.7)
+            {
+                double dist = Math.abs(gamepad2.right_stick_y) * 0.01;
+
+                servo_brat2.setPosition(Range.clip(servo_brat2.getPosition() + dist, 0, 1));
+            }
+
+
+
+
+            if(gamepad2.right_stick_x > 0.01 && servo_brat3.getPosition() >= 0)
+            {
+                double dist = Math.abs(gamepad2.right_stick_x) * 0.01;
+
+                servo_brat3.setPosition(Range.clip(servo_brat3.getPosition() - dist, 0, 1)) ;
+            }
+            else if(gamepad2.right_stick_x < 0.01 && servo_brat3.getPosition() <= 1)
+            {
+                double dist = Math.abs(gamepad2.right_stick_x) * 0.01;
+
+                servo_brat3.setPosition(Range.clip(servo_brat3.getPosition() + dist, 0, 1));
+            }
+
+
 
             double r = Math.hypot(-gamepad1.left_stick_x, gamepad1.left_stick_y);
             double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
             double rightX = -gamepad1.right_stick_x;
-            final double v1 = r * Math.cos(robotAngle) + rightX;
-            final double v2 = r * Math.sin(robotAngle) - rightX;
-            final double v3 = r * Math.sin(robotAngle) + rightX;
-            final double v4 = r * Math.cos(robotAngle) - rightX;
+            motorFrontLeft.setPower(r * Math.cos(robotAngle) + rightX);
+            motorFrontRight.setPower(r * Math.sin(robotAngle) - rightX);
+            motorBackLeft.setPower(r * Math.sin(robotAngle) + rightX);
+            motorBackRight.setPower(r * Math.cos(robotAngle) - rightX);
 
-            motorFrontLeft.setPower(v1);
-            motorFrontRight.setPower(v2);
-            motorBackLeft.setPower(v3);
-            motorBackRight.setPower(v4);
-
-            telemetry.addData("lift pos", lift.getPosition());
+            telemetry.addData("poz lift", lift.getPosition());
+            telemetry.addData("rev poz",  rev_hd_brat.getCurrentPosition());
+            telemetry.addData("servo_brat2 pos: ", servo_brat2.getPosition());
+            telemetry.addData("servo_brat3 pos: ", servo_brat3.getPosition());
             telemetry.update();
         }
 
