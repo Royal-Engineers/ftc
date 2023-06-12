46c719ce8c69a7fe99be4ce97e40a40c45abfb61
Electronica C.N."Stefan cel Mare" Suceava <91566119+ET-CNSCM@users.noreply.github.com>
2023-06-13 01:48:10 +0300

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team20936/teleop/commands/transfer.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.team20936.teleop.commands;
+
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+
+import org.firstinspires.ftc.teamcode.team20936.teleop.subsystems.intakeSubsystem;
+import org.firstinspires.ftc.teamcode.team20936.teleop.subsystems.depositSubsystem;
+
+public class transfer extends SequentialCommandGroup {
+
+    public transfer (intakeSubsystem m_intakeSubsystem, depositSubsystem m_depositSubsystem) {
+        addCommands(
+                new latchOpen(m_depositSubsystem),
+                new setWristRevPos(m_intakeSubsystem, 0.99),
+                new WaitCommand(200),
+                new setArmRevPos(m_intakeSubsystem, 49),
+                new setWristPos(m_intakeSubsystem,0.6244444),
+                new WaitCommand(450),
+                new setClawPos(m_intakeSubsystem,0.15),
+                new WaitCommand(70),
+                new latchClose(m_depositSubsystem),
+                new WaitCommand(300),
+                new setArmRevPos(m_intakeSubsystem,27),
+                new WaitCommand(150),
+                new lowPos(m_intakeSubsystem)
+        );
+        addRequirements(m_intakeSubsystem, m_depositSubsystem);
+    }
+
+}
